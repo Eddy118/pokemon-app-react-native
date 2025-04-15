92ca28a06e8de31992fff060637d9be46e574e86
Eddy118 <42584137+Eddy118@users.noreply.github.com>
2025-04-15 10:21:06 +0100

    Added custom App icons for both android and ios

--- App.tsx
+++ App.tsx
@@ -2,6 +2,10 @@ import MainNavigation from "./src/navigation";
 import { Provider } from "react-redux";
 import {store} from "./src/store/store";
 
+import { LogBox } from 'react-native';
+LogBox.ignoreLogs(['Warning: ...']); // Ignore log notification by message
+LogBox.ignoreAllLogs();
+
 const App = () => {
   return (
     <Provider store={store}>
